4a0647e4ab1b5ba9f81301676889258e2ab3f970
AlanAnibalDSR <alananibaldsr@gmail.com>
2022-04-07 23:42:55 -0700

    fixed inquirer and add another Function

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const inquirer = require("inquirer");
 const generatePage = require('./page-template');
 // const { writeFile, copyFile } = require('./utils/generate-site');
 
-const getEmployeeProperties = () => {
+const getTeamProperties = () => {
     return inquirer.prompt([
         {
           type: 'input',
           name: 'name',
-          message: 'What is your new Team member name? (Required)',
+          message: 'What is the Team manager name? (Required)',
           validate: nameInput => {
             if (nameInput) {
               return true;
@@ -20,55 +20,162 @@ const getEmployeeProperties = () => {
         {
           type: 'input',
           name: 'id',
-          message: 'What is the new team member id number? (Required)',
+          message: 'What is the new team mamager id number? (Required)',
         },
         {
           type: 'input',
           name: 'email',
-          message: 'What is the new team member email address? (Required)',
+          message: 'What is the new team manager email address? (Required)',
+          // validate: function (email) {
+  
+          //   valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+          //   if (valid) {
+          //     console.log("Great job");
+          //       return true;
+          //   } else {
+          //       console.log(".  Please enter a valid email")
+          //       return false;
+          //   }}
+        },
+        {
+          type: 'input',
+          name: 'officeNumber',
+          message: 'What is the new manager office number? (Required)',
+          validate: officeInput => {
+            if (officeInput) {
+              return true;
+            } else {
+              console.log('Please enter a valid office number');
+              return false;
+            }
+          }
         },
         {
             type: 'list',
             name: 'employeeRole',
-            message: 'what is the new employee role',
-            choices: ['Intern', 'Engineer', 'Manager']
-            // if and else
-            
+            message: 'Which Type of Team Member would you like to add?',
+            choices: ['Intern', 'Engineer', 'I don\'t want to add any more team members']
         }
-        // inquire questions
+        
     ])
+    
+    .then(({ employeeRole }) => {
+       
+      // teamData.
+      if(employeeRole === 'Intern') {
+        return internProperties();
+      }
+      if(employeeRole === 'Engineer') {
+        return engineerProperties();
+      }
+      else{
+        console.log('Your Team Profile page is generated');
+      }
+
+    })
 };
 
 const internProperties = () => {
   return inquirer.prompt([
     {
       type: 'input',
-      name: 'school',
-      message: 'What is the new intern School? (Required)',
-    },
-  ])
-};
-
-const managerProperties = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'officeNumber',
-      message: 'What is the new manager office number? (Required)',
-      validate: officeInput => {
-        if (officeInput) {
+      name: 'name',
+      message: 'What is the intern name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
           return true;
         } else {
-          console.log('Please enter a valid office number');
+          console.log('Please enter a valid Name');
           return false;
         }
       }
     },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is the intern id number? (Required)',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is the new intern email address? (Required)',
+      // validate: function (email) {
+
+      //   valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+      //   if (valid) {
+      //     return true;
+      //   } else {
+      //       console.log(".  Please enter a valid email")
+      //       return false;
+      //   }}
+    },   
+    {
+      type: 'input',
+      name: 'school',
+      message: 'What is the intern School? (Required)',
+    },
+    {
+      type: 'list',
+      name: 'employeeRole',
+      message: 'Which Type of Team Member would you like to add?',
+      choices: ['Intern', 'Engineer', 'I don\'t want to add any more team members']
+    }
   ])
+  .then(({ employeeRole }) => {
+       
+    // teamData.
+    if(employeeRole === 'Intern') {
+      return internProperties();
+    }
+    if(employeeRole === 'Engineer') {
+      return engineerProperties();
+    }
+    else{
+      console.log('Your Team Profile page is generated');
+    }
+
+  })
 };
+
+
 
 const engineerProperties = () => {
   return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the engineer name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter a valid Name');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is the engineer id number? (Required)',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is the new engineer email address? (Required)',
+      // validate: function (email) {
+
+      //   valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+      //   if (valid) {
+      //     return true;
+      //   } else {
+      //       console.log(".  Please enter a valid email")
+      //       return false;
+      //   }}
+    }, 
     {
       type: 'input',
       name: 'gitHub',
@@ -82,10 +189,30 @@ const engineerProperties = () => {
         }
       }
     },
+    {
+      type: 'list',
+      name: 'employeeRole',
+      message: 'Which Type of Team Member would you like to add?',
+      choices: ['Intern', 'Engineer', 'I don\'t want to add any more team members']
+    }
   ])
+  .then(({ employeeRole }) => {
+       
+    // teamData.
+    if(employeeRole === 'Intern') {
+      return internProperties();
+    }
+    if(employeeRole === 'Engineer') {
+      return engineerProperties();
+    }
+    else{
+      console.log('Your Team Profile page is generated');
+    }
+
+  })
 };
 
-getEmployeeProperties()
+getTeamProperties()
 .then(answers => {
     console.table(answers);
     
@@ -98,7 +225,7 @@ getEmployeeProperties()
 
 
 
-    // managerProperties()
+    // engineerProperties()
     // .then(answers =>{
     //   console.table(answers);
     // });
@@ -115,5 +242,4 @@ getEmployeeProperties()
     //     });
 
 });
-
 
